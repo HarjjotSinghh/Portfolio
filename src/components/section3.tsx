33ef05cd417ed6857438ca6643cc400f1952298a
Harjot Singh <HarjjotSinghh@gmail.com>
2023-11-23 16:31:21 +0530

    Projects section almost done

--- src/components/section3.tsx
+++ src/components/section3.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Fade } from "react-awesome-reveal"
 import { FADE_IN_ANIMATION_DURATION } from '@/lib/utils'
 import Link from 'next/link'
+import eventopiaImage from "@/../public/eventopia-ss.png"
 import projectsSectionImage from "@/../public/portfolio-website-background-any-section.png"
 import Image from 'next/image'
 import {
@@ -13,6 +14,11 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
+  import { ExternalLinkIcon } from "@radix-ui/react-icons"
+  import MERNLogo from "@/../public/MERN-logo.png"
+  import TailwindLogo from "@/../public/tailwind.png"
+  import TopperHQImage from "@/../public/topperhq-ss.png"
+
 
 export default function Section3() {
   return (
@@ -24,22 +30,58 @@ export default function Section3() {
                     My Projects
                 </h1>
             </Fade>
-            <Fade duration={FADE_IN_ANIMATION_DURATION} triggerOnce className='mt-8 z-10 grid lg:grid-cols-2 grid-cols-1 place-content-center place-items-center'>
-                <div className='w-full'>
-                    <Card className='bg-background/60 backdrop-blur-2xl border-secondary hover:border-accent hover:bg-background/80 transition-all ease-in-out duration-300'>
+            <Fade duration={FADE_IN_ANIMATION_DURATION} triggerOnce>
+                
+                <div className='pt-16 z-10 grid lg:grid-cols-2 grid-cols-1 place-content-center place-items-center lg:grid-flow-col grid-flow-row gap-16'>
+                    <Card className='bg-background/20 backdrop-blur-2xl border-secondary hover:border-accent hover:bg-background/50 transition-all ease-in-out duration-300 shadow-[0px_15px_50px] shadow-secondary/70 hover:shadow-primary/20'>
                         <CardHeader>
-                            <CardTitle className='text-2xl'>Eventopia</CardTitle>
+                            <CardTitle className='xl:text-4xl text-2xl'>
+                                <Link rel='noopener' target='_blank' href={"https://eventopia.vercel.app"} className='underline-offset-auto underline decoration-primary decoration-2 hover:decoration-accent transition-all duration-300 ease-in-out inline-flex items-center gap-2 lg:gap-3'>
+                                    Eventopia <ExternalLinkIcon className='lg:w-6 lg:h-6 w-4 h-4 select-none' />
+                                </Link>
+                            </CardTitle>
                         </CardHeader>
                         <CardContent>
-                            <Image src={projectsSectionImage} alt='' className='aspect-video'></Image>
-                            <CardDescription className='text-foreground/60 w-full mt-4'>A website that provides comprehensive listings of current, upcoming, and past college/university events taking place in New Delhi.</CardDescription>
+                            <Image src={eventopiaImage} alt='' className='object-contain select-none' draggable={false}></Image>
+                            <CardDescription className='text-foreground/80 w-full mt-4 xl:text-lg'>A website that provides comprehensive listings of current, upcoming, and past college/university events taking place in New Delhi.</CardDescription>
                         </CardContent>
                         <CardFooter>
-                            <p>Card Footer</p>
+                            <div className='flex justify-start items-start flex-row w-full'>
+                                <div className='flex flex-col items-center justify-center gap-8 w-full'>
+                                    <h1 className='xl:text-2xl text-xl w-full text-center'>Tech stack used</h1>
+                                    <div className='flex gap-4 items-center'>
+                                        <Image src={MERNLogo}  draggable={false} alt='MERN Stack Logo' className='select-none xl:h-[100px] h-[60px] w-auto hover:scale-105 transition-all duration-300 ease-in-out'></Image>
+                                        <Image src={TailwindLogo}  draggable={false} alt='Tailwind CSS Logo' className='select-none xl:h-[50px] h-[30px] w-auto hover:scale-105 transition-all duration-300 ease-in-out'></Image>
+                                    </div>
+                                </div>
+                            </div>
+                        </CardFooter>
+                    </Card>
+                    <Card className='bg-background/20 backdrop-blur-2xl border-secondary hover:border-accent hover:bg-background/50 transition-all ease-in-out duration-300 shadow-[0px_15px_50px] shadow-secondary/70 hover:shadow-primary/20'>
+                        <CardHeader>
+                            <CardTitle className='xl:text-4xl text-2xl'>
+                                <Link rel='noopener' target='_blank' href={"https://topper-hq.vercel.app"} className='underline-offset-auto underline decoration-primary decoration-2 hover:decoration-accent transition-all duration-300 ease-in-out inline-flex items-center gap-2 lg:gap-3'>
+                                    TopperHQ <ExternalLinkIcon className='lg:w-6 lg:h-6 w-4 h-4 select-none' />
+                                </Link>
+                            </CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <Image src={TopperHQImage} alt='' className='object-contain select-none' draggable={false}></Image>
+                            <CardDescription className='text-foreground/80 w-full mt-4 xl:text-lg'>A website which displays all important question/answers from each chapter of every subject in a summarised and easy to remember format.</CardDescription>
+                        </CardContent>
+                        <CardFooter>
+                            <div className='flex justify-start items-start flex-row w-full'>
+                                <div className='flex flex-col items-center justify-center gap-8 w-full'>
+                                    <h1 className='xl:text-2xl text-xl w-full text-center'>Tech stack used</h1>
+                                    <div className='flex gap-4 items-center'>
+                                        <Image src={MERNLogo}  draggable={false} alt='MERN Stack Logo' className='select-none xl:h-[100px] h-[60px] w-auto hover:scale-105 transition-all duration-300 ease-in-out'></Image>
+                                        <Image src={TailwindLogo}  draggable={false} alt='Tailwind CSS Logo' className='select-none xl:h-[50px] h-[30px] w-auto hover:scale-105 transition-all duration-300 ease-in-out'></Image>
+                                    </div>
+                                </div>
+                            </div>
                         </CardFooter>
                     </Card>
                 </div>
-                
             </Fade>
         </div>
         <div className='absolute lg:min-w-screen lg:h-auto min-h-screen w-full select-none overflow-hidden -z-30'>
